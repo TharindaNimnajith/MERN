fe8cae92579e72c2791357920dbcaba8ce200c2f
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-02-02 13:32:53 +0530

    crud

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -15,6 +15,7 @@ router.route('/').get((req, res) => {
 
 // route (end point) that handles incoming http get requests
 // if the url is http://localhost:5000/exercises/add and if it is a post request internal code will be executed
+// insert
 router.route('/add').post((req, res) => {
     // assign different parts of the request body to several variables
     const username = req.body.username;
@@ -32,6 +33,44 @@ router.route('/add').post((req, res) => {
 
     newExercise.save()  // save the new exercise to the database
         .then(() => res.json('Exercise added!'))  // if successful, return success message in json format
+        .catch(err => res.status(400).json('Error: ' + err));  // in case of an error, return status 400 with error message in json format
+});
+
+// retrieve
+// :id is like a variable (an objectId created automatically by mongodb)
+// if the url is http://localhost:5000/exercises/<objectId from the database> and if it is a get request internal code will be executed
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)  // get the id directly from the url and find the relevant Exercise
+        .then(exercise => res.json(exercise))  // once getting the relevant exercise, return that exercise in json format
+        .catch(err => res.status(400).json('Error: ' + err));  // in case of an error, return status 400 with error message in json format
+});
+
+// delete
+// :id is like a variable (an objectId created automatically by mongodb)
+// if the url is http://localhost:5000/exercises/<objectId from the database> and if it is a delete request internal code will be executed
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)  // get the id directly from the url, find and delete the relevant Exercise
+        .then(() => res.json('Exercise deleted!'))  // if successful, return success message in json format
+        .catch(err => res.status(400).json('Error: ' + err));  // in case of an error, return status 400 with error message in json format
+});
+
+// update
+// :id is like a variable (an objectId created automatically by mongodb)
+// if the url is http://localhost:5000/exercises/update/<objectId from the database> and if it is a post request internal code will be executed
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)  // get the id directly from the url and find the relevant Exercise
+
+        // set different parts of the request body to the relevant fields of the found exercise
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()  // save the exercise with new information to the database
+                .then(() => res.json('Exercise updated!'))  // if successful, return success message in json format
+                .catch(err => res.status(400).json('Error: ' + err));  // in case of an error, return status 400 with error message in json format
+        })
         .catch(err => res.status(400).json('Error: ' + err));  // in case of an error, return status 400 with error message in json format
 });
 

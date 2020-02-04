2a16cd46edc7cf2238db9f03ca66a7cb030df63c
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-02-04 19:49:15 +0530

    build

---
+++ mern-exercise-tracker/build/precache-manifest.26dfc0df3dfa02756219b535e0601185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acdc4f54d2cd1c0e8e9533e6a36c9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fbcc6e239917a93f213",
+    "url": "/static/css/main.1c4f1d86.chunk.css"
+  },
+  {
+    "revision": "65c002a7a6390a1e8efa",
+    "url": "/static/js/2.1c533cc4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1c533cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbcc6e239917a93f213",
+    "url": "/static/js/main.37c4cfe6.chunk.js"
+  },
+  {
+    "revision": "1809a694a46bf86e0e94",
+    "url": "/static/js/runtime-main.24340f72.js"
+  }
+]);
--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -25,8 +25,6 @@ function App() {
 				  </header>
 				*/
 			}
-
-			
 		</div>
 	);
 }

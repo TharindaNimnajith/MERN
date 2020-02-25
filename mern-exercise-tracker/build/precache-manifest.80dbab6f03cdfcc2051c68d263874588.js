61045a34efda160746b530672a151db393d780b4
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-02-25 20:44:46 +0530

    react

---
+++ mern-exercise-tracker/build/precache-manifest.80dbab6f03cdfcc2051c68d263874588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37fe493da0c4df3eba8a8c2b5781b279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c5dcd6d1171f0e8511",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "79ae833c8d6e48f032c2",
+    "url": "/static/css/main.8a43f95b.chunk.css"
+  },
+  {
+    "revision": "22c5dcd6d1171f0e8511",
+    "url": "/static/js/2.0d5e0a6e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0d5e0a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ae833c8d6e48f032c2",
+    "url": "/static/js/main.75541244.chunk.js"
+  },
+  {
+    "revision": "1809a694a46bf86e0e94",
+    "url": "/static/js/runtime-main.24340f72.js"
+  }
+]);
--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,36 +1,34 @@
 /* jshint esversion: 6 */
 
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";  // import react router for easy routing
+import "bootstrap/dist/css/bootstrap.min.css";  // import bootstrap for styling
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+// import components
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import UsersList from "./components/users-list.component";
+import EditUser from "./components/edit-user.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
 	return (
-		<div className="App">
-			{
-				/*
-					<header className="App-header">
-						<img src={logo} className="App-logo" alt="logo" />
+        <Router>
+            <Navbar />
+            <br />
 
-						<p>
-							Edit <code>src/App.js</code> and save to reload.
-						</p>
-
-						<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-						>
-							Learn React
-						</a>
-					</header>
-				*/
-			}
-
-			
-		</div>
-	);
+            <Route path="/exercises" exact component={ExercisesList} />
+            <Route path="/edit_exercise/:id" exact component={EditExercise} />
+            <Route path="/create_exercise" exact component={CreateExercise} />
+            <Route path="/users" exact component={UsersList} />
+            <Route path="/edit_user/:id" exact component={EditUser} />
+            <Route path="/create_user" exact component={CreateUser} />
+        </Router>
+    );
 }
 
 export default App;
